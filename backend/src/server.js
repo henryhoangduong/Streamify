e7e6e3412d78f51d78049e61d2317ef5784eabfd
henryhoangduong <henryhoangduong@gmail.com>
2025-08-30 08:44:34 +0700

    backend: corse

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,16 @@ import { router as AuthRoutes } from "./routes/auth.route.js";
 import { router as UserRoutes } from "./routes/user.route.js";
 import { router as ChatRoutes } from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 const app = express();
 
 const PORT = process.env.PORT;
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
 app.use(express.json());
 app.use("/api/auth", AuthRoutes);
 app.use("/api/user", UserRoutes);
