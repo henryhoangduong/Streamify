6e45397c107b48805633620365b7c5a4903b1f1b
henryhoangduong <henryhoangduong@gmail.com>
2025-09-07 20:52:24 +0700

    backend: logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -95,7 +95,7 @@ export async function login(req, res) {
 
 export function logout(req, res) {
   res.clearCookie("jwt");
-  res.status(200).json({ succes: true, message: "Successful logout" });
+  res.status(200).json({ success: true, message: "Logout successful" });
 }
 
 export async function onboard(req, res) {
